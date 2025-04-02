8569807d8405de8c94386560311db35569a34c73
Rango Mango <mangorango144@gmail.com>
2025-04-02 19:57:23 +0300

    refactor: restructure useAuth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,41 +1,19 @@
-import { useState, useEffect } from "react";
 import { auth, googleProvider } from "../config";
 import {
-  onAuthStateChanged,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   signOut,
-  User,
   signInWithRedirect,
   UserCredential,
 } from "firebase/auth";
 
 export const useAuth = () => {
-  // User state with Firebase User type
-  const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  // Listen for auth state changes
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
-    });
-
-    return () => unsubscribe(); // Clean up the listener
-  }, []);
-
   const signUp = async (
     email: string,
     password: string
   ): Promise<UserCredential> => {
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      return userCredential;
+      return await createUserWithEmailAndPassword(auth, email, password);
     } catch (error: any) {
       console.error("Error signing up:", error.message);
       throw error;
@@ -47,12 +25,7 @@ export const useAuth = () => {
     password: string
   ): Promise<UserCredential | null> => {
     try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      return userCredential;
+      return await signInWithEmailAndPassword(auth, email, password);
     } catch (error: any) {
       console.error("Error signing in:", error.message);
       return null;
@@ -75,5 +48,8 @@ export const useAuth = () => {
     }
   };
 
-  return { user, loading, signUp, signIn, signInWithGoogle, logOut };
+  // Expose logOut globally for testing
+  (window as any).logOut = logOut;
+
+  return { signUp, signIn, signInWithGoogle, logOut };
 };
