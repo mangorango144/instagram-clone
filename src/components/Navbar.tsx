379e4121aeb3268f5cc1af6ee601e93d71a769b2
Rango Mango <mangorango144@gmail.com>
2025-04-08 20:53:25 +0300

    refactor: clean up and streamline navbar code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,19 @@ import { SlMagnifier } from "react-icons/sl";
 import { GrWaypoint } from "react-icons/gr";
 import { FaRegHeart } from "react-icons/fa";
 import { FiPlusSquare } from "react-icons/fi";
+import { CgDetailsMore } from "react-icons/cg";
+import { Link } from "react-router-dom";
 
 const navClassName = `
   md:block md:top-0 bottom-0 left-0 fixed flex bg-black 
   border-stone-800 border-t-[1px] md:border-r-[1px] 
   w-screen md:w-[70px] 2xl:w-[270px] xl:w-[220px] 
-  h-[48px] md:h-screen
+  h-[48px] md:h-screen text-white
 `;
 
-const h1ClassName = `
+const navbarLogoClass = `
   hidden lg:flex md:justify-center xl:justify-start items-center 
-  xl:px-8 py-10 font-vibes font-extrabold text-white text-4xl
+  xl:px-8 py-10 font-vibes font-extrabold text-4xl
 `;
 
 const ulClassName = `
@@ -23,41 +25,70 @@ const ulClassName = `
   md:w-auto md:h-screen lg:h-auto
 `;
 
+const iconClassName = "xl:mr-4 text-2xl";
+
+const navItems = [
+  {
+    label: "Home",
+    icon: <GrHomeRounded className={iconClassName} />,
+    to: "/",
+  },
+  { label: "Search", icon: <SlMagnifier className={iconClassName} /> },
+  { label: "Messages", icon: <GrWaypoint className={iconClassName} /> },
+  { label: "Notifications", icon: <FaRegHeart className={iconClassName} /> },
+  { label: "Create", icon: <FiPlusSquare className={iconClassName} /> },
+  {
+    label: "Profile",
+    icon: (
+      <div className="bg-stone-500 xl:mr-4 rounded-full outline-2 w-6 h-6" />
+    ),
+    to: "/johndoe",
+    extraClasses: "font-bold",
+  },
+];
+
 export function Navbar() {
   return (
     <nav className={navClassName}>
-      <h1 className={h1ClassName}>
-        <FaInstagram className="xl:hidden inline hover:cursor-pointer" />
-        <span className="hidden xl:inline hover:cursor-pointer">
-          InstaClone
-        </span>
-      </h1>
-      <ul className={ulClassName}>
-        <li className="flex items-center md:hover:bg-stone-800 p-3 text-white hover:cursor-pointer">
-          <GrHomeRounded className="xl:mr-4 text-2xl" />
-          <span className="hidden xl:inline">Home</span>
-        </li>
-        <li className="flex justify-center md:items-center md:hover:bg-stone-800 p-3 text-white hover:cursor-pointer">
-          <SlMagnifier className="xl:mr-4 text-2xl" />
-          <span className="hidden xl:inline">Search</span>
-        </li>
-        <li className="flex justify-center md:items-center md:hover:bg-stone-800 p-3 text-white hover:cursor-pointer">
-          <GrWaypoint className="xl:mr-4 text-2xl" />
-          <span className="hidden xl:inline">Messages</span>
-        </li>
-        <li className="flex justify-center md:items-center md:hover:bg-stone-800 p-3 text-white hover:cursor-pointer">
-          <FaRegHeart className="xl:mr-4 text-2xl" />
-          <span className="hidden xl:inline">Notifications</span>
-        </li>
-        <li className="flex justify-center md:items-center md:hover:bg-stone-800 p-3 text-white hover:cursor-pointer">
-          <FiPlusSquare className="xl:mr-4 text-2xl" />
-          <span className="hidden xl:inline">Create</span>
-        </li>
-        <li className="flex justify-center md:items-center md:hover:bg-stone-800 p-3 font-bold text-white hover:cursor-pointer">
-          <div className="bg-stone-500 xl:mr-4 rounded-full outline-2 w-6 h-6"></div>
-          <span className="hidden xl:inline">Profile</span>
-        </li>
-      </ul>
+      <div className="flex flex-col justify-between w-full h-full">
+        <div>
+          <Link to="/" className={navbarLogoClass}>
+            <FaInstagram className="xl:hidden inline hover:cursor-pointer" />
+            <span className="hidden xl:inline hover:cursor-pointer">
+              InstaClone
+            </span>
+          </Link>
+
+          <ul className={ulClassName}>
+            {navItems.map(({ label, icon, to, extraClasses = "" }, index) => {
+              const classes = `flex justify-center xl:justify-start items-center md:hover:bg-stone-800 p-3 w-full hover:cursor-pointer ${extraClasses}`;
+              const content = (
+                <>
+                  {icon}
+                  <span className={`hidden xl:inline ${extraClasses}`}>
+                    {label}
+                  </span>
+                </>
+              );
+
+              return to ? (
+                <Link key={index} to={to} className={classes}>
+                  {content}
+                </Link>
+              ) : (
+                <li key={index} className={classes}>
+                  {content}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="hidden xl:flex justify-start md:items-center md:hover:bg-stone-800 m-4 p-3 hover:cursor-pointer">
+          <CgDetailsMore className="xl:mr-4 text-3xl" />
+          More
+        </div>
+      </div>
     </nav>
   );
 }
