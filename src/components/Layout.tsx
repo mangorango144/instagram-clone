05bc4443707459db98d0c134914615f2523912cf
Rango Mango <mangorango144@gmail.com>
2025-04-09 17:58:11 +0300

    feat: show Navbar only when user is authenticated

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,23 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 export const Layout = () => {
+  const { uid } = useSelector((state: RootState) => state.auth);
+  const isAuthenticated = !!uid;
+
   return (
     <div className="flex md:flex-row flex-col min-h-screen">
-      <Navbar />
+      {isAuthenticated && <Navbar />}
       {/* Margin for the main content to prevent overlap with the navbar */}
-      <main className="flex-1 mb-[70px] md:ml-[70px] 2xl:ml-[270px] xl:ml-[220px]">
+      <main
+        className={
+          isAuthenticated
+            ? "flex-1 mb-[70px] md:ml-[70px] 2xl:ml-[270px] xl:ml-[220px]"
+            : "flex-1"
+        }
+      >
         <Outlet />
       </main>
     </div>
